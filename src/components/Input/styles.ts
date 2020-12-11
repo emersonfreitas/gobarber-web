cf8a4b43c21ac9128019eed94110922c6727952f
Emerson Freitas <emersonfreitasn@gmail.com>
2020-12-11 14:13:43 -0500

    creating error tooltip

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,9 +1,12 @@
 /* eslint-disable prettier/prettier */
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../Tooltip';
+
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -21,6 +24,10 @@ export const Container = styled.div<ContainerProps>`
   & + div {
     margin-top: 8px;
   }
+
+  ${(props) => props.isErrored && css`
+      border-color: #c53030;
+  `}
 
   ${(props) => props.isFocused && css`
       color: #ff9000;
@@ -44,5 +51,23 @@ export const Container = styled.div<ContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #FFF;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
